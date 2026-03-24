673ced708982e98c60428586d065dbe44b96a812
jimbeann <sidd.shekhar786@gmail.com>
2026-03-24 11:21:15 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from app.agent import analyze_stock
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/analyze", response_class=HTMLResponse)
+async def analyze(request: Request, stock: str = Form(...)):
+    result = analyze_stock(stock)
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "result": result,
+        "stock": stock
+    })
